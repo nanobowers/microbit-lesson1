8e4d2c02c659427e999ee644782b6776f55a4d90
nanobowers <nanobowers@gmail.com>
2021-01-06 16:56:01 -0500

    radio

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(" ROXXANNNEEEE")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
